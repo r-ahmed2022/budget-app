83af06328d04200d51eb8a9197a04700d0409e25
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-07 23:52:06 +0530

    created splash screen and corresponding controller

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,8 @@
+class SplashController < ApplicationController
+    def index
+    end
+   private
+   def redirect_authenticated_user
+       redirect_to categories_path if user_signed_in?
+     end
+end
