ced0f9b1ec42371336f71c916287a2011c25e08f
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-08 20:42:41 +0530

    updated ability authorisations

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user || User.new
+    if user.is?(:admin)
+      can :manage, :all
+    else
+      can :manage, Category, author_id: user.id
+      can :create, Category, author_id: user.id
+      can :edit, Category, author_id: user.id
+      can :destroy, Entity, author_id: user.id
+      can :manage, Entity, author_id: user.id
+      can :create, Entity, author_id: user.id
+      can :edit, Entity, author_id: user.id
+      can :destroy, Entity, author_id: user.id
+      can :read, :all
+    end
+  end
+end
