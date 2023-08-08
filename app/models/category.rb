5fd6911baf9bb66b2393cf0c841d9932eb54a8ca
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-08 15:57:54 +0530

    setup category controller, model, and view

---
+++ app/models/category.rb
@@ -0,0 +1,20 @@
+class Category < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  #has_many :category_entities, foreign_key: 'category_id', class_name: 'CategoryEntity', dependent: :delete_all
+
+  validates :name, presence: true
+  validates :icon, presence: true
+  validates :total_amount, numericality: { greater_than_or_equal_to: 0 }
+  before_validation :set_default_value
+  def date
+    "#{created_at.day}/#{created_at.month}/#{created_at.year}"
+  end
+
+  def total
+    "$#{total_amount}"
+  end
+
+  def set_default_value
+    self.total_amount ||= 0.0
+  end
+end
